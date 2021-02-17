db9c216eee5eebd77975494b2daf9d6eff5745f2
Bruna Ramos <75384351+bruramos@users.noreply.github.com>
2021-02-17 19:24:48 -0300

    Add files via upload

---
+++ lacos_e_repeticoes/retangulos.py
@@ -0,0 +1,13 @@
+
+l = int(input("digite a largura: "))
+a = int(input("digite a altura: "))
+auxl = l
+while a >= 1:
+    while auxl >=1:
+        print("#", end = "")
+        auxl = auxl - 1
+    print("")
+    auxl = l    
+    a = a - 1
+    
+    
